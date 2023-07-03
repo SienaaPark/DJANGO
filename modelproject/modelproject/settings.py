9d1009c6d5b1ade2e4f6e46f5753f006f41a7c46
SIENA <siena@SIENAui-MacBookAir.local>
2023-07-03 16:03:26 +0900

    commit message

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'modelproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'drf_test',
+        'USER': 'root',
+        'PASSWORD': '9788',
+        'HOST': 'svc.sel3.cloudtype.app',
+        'PORT': ''
     }
 }
 
